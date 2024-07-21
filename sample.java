a3704c707405075de1ac02144f4a6c9267d84608
SujanShetty003 <sujanshetty003@gmail.com>
2024-07-21 19:08:39 +0530

    file is javajcvjdgg

--- sample.java
+++ sample.java
@@ -1,4 +1,5 @@
-public class Sample{
-  public static void main(String[] args){
-    System.out.println("Hello World");
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
